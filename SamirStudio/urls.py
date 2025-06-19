460c4319c5975d46321f13ced35ff5d4f8c00f9c
Deviicho <devviicho@gmail.com>
2025-06-19 20:24:51 +0300

    procfile + urls debug

--- SamirStudio/urls.py
+++ SamirStudio/urls.py
@@ -29,11 +29,13 @@ def dbcheck(request):
     except Exception as e:
         return JsonResponse({"db": "error", "detail": str(e)})
 
-urlpatterns += [
-    path("dbcheck/", dbcheck)
-]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path("dbcheck/", dbcheck)
+]
